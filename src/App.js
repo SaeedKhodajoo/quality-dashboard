11a7b7c70a96d235cffb3d4fc140917ee85a95cf
unknown <saeedkhodajoo1995@gmail.com>
2022-02-19 16:14:00 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "roboto",
+      }}
+    >
+      App
+    </div>
+  );
+};
+
+export default App;
